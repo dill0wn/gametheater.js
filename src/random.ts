b1a2674683dceccf6b1f9fca280d72eee6525801
dill0wn <dillon.walls@slashdotmedia.com>
2022-04-13 19:53:31 -0400

    added rand_range_int

--- src/random.ts
+++ src/random.ts
@@ -1,3 +1,7 @@
 export function rand_range(min: number, max: number) {
     return min + Math.random() * (max - min);
 }
+
+export function rand_range_int(min: number, max: number) {
+    return Math.floor(rand_range(min, max));
+}
